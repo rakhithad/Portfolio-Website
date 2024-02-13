05bd482e8d51ed70d6b6939de1948e6103310cc1
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2024-02-13 23:49:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+var sidemenu = document.getElementById("sidemenu");
+
+function opentab(event, tabname) {
+    console.log('E: ', event)
+    console.log('T : ', tabname)
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
